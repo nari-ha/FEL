9178b1d7132aeecb95725b557e3edc3ce6db1cd0
nari-ha <narihanar@gmail.com>
2025-03-11 14:26:32 +0000

    stage2 그래프 그리기 수정

--- processor/processor_clipreid_stage2.py
+++ processor/processor_clipreid_stage2.py
@@ -156,12 +156,12 @@ def do_train_stage2(cfg,
 
     ax1.set_xlabel("Epoch")
     ax1.set_ylabel("Loss", color='blue')
-    ax1.plot(range(1, epochs + 1), loss_history, label="Loss", color='blue', linewidth=2)
+    ax1.plot(range(1, len(loss_history) + 1), loss_history, label="Loss", color='blue', linewidth=2)
     ax1.tick_params(axis='y', labelcolor='blue')
 
     ax2 = ax1.twinx()
     ax2.set_ylabel("Accuracy", color='orange')
-    ax2.plot(range(1, epochs + 1), accuracy_history, label="Accuracy", color='orange', linewidth=2)
+    ax2.plot(range(1, len(accuracy_history) + 1), accuracy_history, label="Accuracy", color='orange', linewidth=2)
     ax2.tick_params(axis='y', labelcolor='orange')
 
     fig.suptitle("Stage2 Loss&Accuracy")
@@ -175,12 +175,12 @@ def do_train_stage2(cfg,
 
     ax1.set_xlabel("eval steps")
     ax1.set_ylabel("mAP")
-    ax1.plot(range(1, (epochs/eval_period) + 1), loss_history, label="mAP", color='red', linewidth=2)
+    ax1.plot(range(1, len(loss_history) + 1), map_history, label="mAP", color='red', linewidth=2)
     ax1.tick_params(axis='y', labelcolor='red')
 
     ax2 = ax1.twinx()
     ax2.set_ylabel("R1")
-    ax2.plot(range(1, (epochs/eval_period) + 1), accuracy_history, label="R1", color='green', linewidth=2)
+    ax2.plot(range(1, len(loss_history) + 1), r1_history, label="R1", color='green', linewidth=2)
     ax2.tick_params(axis='y', labelcolor='green')
 
     fig.suptitle("Stage2 Evaluation")
