02887db0ee77d3151c9f01b1b8c82c77cf0f7683
nari-ha <narihanar@gmail.com>
2025-04-01 15:22:23 +0100

    modified:   solver/make_optimizer_prompt.py

--- solver/make_optimizer_prompt.py
+++ solver/make_optimizer_prompt.py
@@ -10,6 +10,11 @@ def make_optimizer_1stage(cfg, model):
             weight_decay = cfg.SOLVER.STAGE1.WEIGHT_DECAY
             params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
             keys += [key]
+    for k, v in model.named_parameters():
+        if v.requires_grad:
+            print(f"Trainable: {k}")
+        else:
+            print(f"Frozen: {k}")
     if cfg.SOLVER.STAGE1.OPTIMIZER_NAME == 'SGD':
         optimizer = getattr(torch.optim, cfg.SOLVER.STAGE1.OPTIMIZER_NAME)(params, momentum=cfg.SOLVER.STAGE1.MOMENTUM)
     elif cfg.SOLVER.STAGE1.OPTIMIZER_NAME == 'AdamW':
