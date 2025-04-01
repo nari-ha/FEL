0ceda048a5377f1cd48ee0d07ae638d793805909
nari-ha <narihanar@gmail.com>
2025-04-01 15:59:10 +0100

    .

--- solver/make_optimizer_prompt.py
+++ solver/make_optimizer_prompt.py
@@ -8,9 +8,6 @@ def make_optimizer_1stage(cfg, model):
         if "text_encoder" in key:
             value.requires_grad_(False)
             continue   
-        if "image_encoder" in key:
-            value.requires_grad_(False)
-            continue
         if "feature_enhancer_layer" in key:
             value.requires_grad_(False)
             continue
@@ -47,9 +44,6 @@ def make_optimizer_2stage(cfg, model, center_criterion):
         # fel freeze
         if "prompt_learner" in key:
             value.requires_grad_(False)
-            continue
-        if "image_encoder" in key:
-            value.requires_grad_(True)
             continue
         if not value.requires_grad:
             continue
