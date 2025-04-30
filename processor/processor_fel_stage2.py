c3d5217a1957f39894ae3563f7ae5ebd8c72e303
nari-ha <narihanar@gmail.com>
2025-04-30 16:21:29 +0100

    .

--- processor/processor_fel_stage2.py
+++ processor/processor_fel_stage2.py
@@ -107,8 +107,6 @@ def do_train_stage2(cfg,
             with amp.autocast(enabled=True):
                 score, feat, image_features = model(x = img, label = target, cam_label=target_cam, view_label=target_view)
                 logits = image_features @ text_features.t()
-                print("img_features: ", image_features.size())
-                print("txt_features: ", text_features.size())
                 loss = loss_fn(score, feat, target, target_cam, logits)
 
             scaler.scale(loss).backward()
