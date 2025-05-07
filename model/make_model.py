1c39babf414c75af5d8855d15e87bd9d34e44541
nari-ha <narihanar@gmail.com>
2025-05-07 14:49:04 +0100

    .

--- model/make_model.py
+++ model/make_model.py
@@ -107,11 +107,11 @@ class build_transformer(nn.Module):
         padded_tokens = tokens + [0] * (77 - len(tokens))
         text = torch.tensor([padded_tokens]).cuda()
         text_features = self.clip_model.encode_text(text)
-        bp()
         text_features = text_features.repeat(img_feature_proj.size()[0], 1)
-        text_features = text_features.unsqueeze(1)
-        img_feature_proj = img_feature_proj.unsqueeze(1)  # [B, 1, D]
-        
+        text_features = text_features.squeeze(1) # [B, D]
+        # text_features = text_features.unsqueeze(1) # [B, 1, D]
+        # img_feature_proj = img_feature_proj.unsqueeze(1) # [B, D]
+        bp()
         img_feature_proj, text_features = self.feature_enhancer_layer(
             v=img_feature_proj, l=text_features, attention_mask_v=None, attention_mask_l=None
         )
