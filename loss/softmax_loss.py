e84d3a19cb1110d8dbe74ac9c335d9800d99fa0c
nari-ha <narihanar@gmail.com>
2025-05-07 14:29:02 +0100

    try

--- loss/softmax_loss.py
+++ loss/softmax_loss.py
@@ -33,8 +33,12 @@ class CrossEntropyLabelSmooth(nn.Module):
         log_probs = self.logsoftmax(inputs)
         # print("log_probs: ", log_probs.size())
         # bp()
+        try:
+            targets2 = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
+        except:
+            bp()
         # targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1) 
-        targets2 = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1) 
+        
         if self.use_gpu: targets2 = targets2.cuda()
         targets3 = (1 - self.epsilon) * targets2 + self.epsilon / self.num_classes
         loss = (- targets3 * log_probs).mean(0).sum()
