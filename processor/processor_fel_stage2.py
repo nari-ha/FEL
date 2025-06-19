3ae204fd2f092e3a369e72ac8429c8740bf259d0
nari-ha <narihanar@gmail.com>
2025-06-20 00:18:12 +0100

    .

--- processor/processor_fel_stage2.py
+++ processor/processor_fel_stage2.py
@@ -170,7 +170,7 @@ def evaluate_model(cfg, model, val_loader, evaluator, device, epoch, logger):
             img = img.to(device)
             camids = camids.to(device) if cfg.MODEL.SIE_CAMERA else None
             target_view = target_view.to(device) if cfg.MODEL.SIE_VIEW else None
-            img_feat, txt_feat = model(img, cam_label=camids, view_label=target_view, get_feat = True)
+            img_feat = model(img, cam_label=camids, view_label=target_view, get_feat = True)
             evaluator.update((img_feat, vid, camid))
     
     cmc, mAP, _, _, _, _, _ = evaluator.compute()
