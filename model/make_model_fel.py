e482cd9ecf9a9cbd765d13d9c590c6d9604f7066
nari-ha <narihanar@gmail.com>
2025-06-25 15:11:27 +0100

    .

--- model/make_model_fel.py
+++ model/make_model_fel.py
@@ -218,9 +218,6 @@ class build_transformer(nn.Module):
             # img_feature_proj = img_feature_proj.unsqueeze(1)  # [B, 1, D]
             
             v_feature = torch.stack([img_feat, img_feature_last, img_feature_proj], dim=1)
-            if v_feature.dtype == torch.float32:
-                bp()
-                v_feature = v_feature.half()
             print(f"v_type: {v_feature.dtype} l_type: {l_feature.dtype}")
             img_features, text_features = self.feature_enhancer_layer1(v=v_feature, l=l_feature, attention_mask_v=None, attention_mask_l=None)
             # img_features, text_features = self.feature_enhancer_layer(
--- processor/processor_fel_stage2.py
+++ processor/processor_fel_stage2.py
@@ -171,7 +171,7 @@ def evaluate_model(cfg, model, val_loader, evaluator, device, epoch, logger, tex
             img = img.to(device)
             camids = camids.to(device) if cfg.MODEL.SIE_CAMERA else None
             target_view = target_view.to(device) if cfg.MODEL.SIE_VIEW else None
-            img_feat = model(img, cam_label=camids, view_label=target_view, t_feat = text_features)
+            img_feat = model(img, cam_label=camids, view_label=target_view, t_feat = text_features.float())
             evaluator.update((img_feat, vid, camid))
     
     cmc, mAP, _, _, _, _, _ = evaluator.compute()
