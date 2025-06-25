e30ae61471db13dc69db1ca93c206a6d60123a3b
nari-ha <narihanar@gmail.com>
2025-06-25 15:48:04 +0100

    .

--- model/make_model_fel.py
+++ model/make_model_fel.py
@@ -218,7 +218,7 @@ class build_transformer(nn.Module):
             # img_feature_proj = img_feature_proj.unsqueeze(1)  # [B, 1, D]
             
             v_feature = torch.stack([img_feat, img_feature_last, img_feature_proj], dim=1)
-            print(f"v_type: {v_feature.dtype} l_type: {l_feature.dtype}")
+            # print(f"v_type: {v_feature.dtype} l_type: {l_feature.dtype}")
             img_features, text_features = self.feature_enhancer_layer1(v=v_feature, l=l_feature, attention_mask_v=None, attention_mask_l=None)
             # img_features, text_features = self.feature_enhancer_layer(
             #     v=v_feature, l=l_feature, attention_mask_v=None, attention_mask_l=None
